1b30ab5d8cd75146f8dee1159f201588801dc563
Joanna Murawska <jkmurek@gmail.com>
2025-11-03 15:33:29 +0100

    Initial commit

---
+++ src/main/java/com/example/phonebook/PhonebookBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookBeApplication.class, args);
+	}
+
+}
