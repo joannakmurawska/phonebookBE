e084267d613c0c35f34382dc990bf8099418a2cf
Joanna Murawska <jkmurek@gmail.com>
2025-11-03 18:41:32 +0100

    Fix cors issue on prod

--- src/main/java/com/example/phonebook/config/WebConfig.java
+++ src/main/java/com/example/phonebook/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:*")
+                        .allowedOriginPatterns("http://localhost:*", "https://phonebookfe.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
