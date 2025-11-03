e0107fca0286487763e6b3b01119fded94e56d53
Joanna Murawska <jkmurek@gmail.com>
2025-11-03 15:46:59 +0100

    Connection to DB works

---
+++ src/main/java/com/example/phonebook/entity/PhoneNumber.java
@@ -0,0 +1,27 @@
+package com.example.phonebook.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "phonebook")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhoneNumber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
+}
---
+++ src/main/java/com/example/phonebook/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.phonebook.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PhoneNumber> phoneNumbers;
+}
